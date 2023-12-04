eace39dcbd1545cd8601c19fb71d13eed4baa3f2
w4ty <therex102@gmail.com>
2023-12-04 20:03:08 +0100

    Include favicon and css

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("./src/css/");
+    eleventyConfig.addWatchTarget("./src/css/");
+    eleventyConfig.addPassthroughCopy("./src/images/");
+    eleventyConfig.addPassthroughCopy({ "./src/favicons": "/" });
+    
     return {
         dir: {
             input: "src",
